362038502796c38ebb6d652d453b516d61795243
Frank <frank.martinez.27@outlook.com>
2019-09-28 18:36:56 -0700

    First commit

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,5 +1,5 @@
 import * as Expo from "expo";
-import { Font } from 'expo';
+import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons';
 import React, { Component } from "react";
 import { StyleProvider } from "native-base";
@@ -20,18 +20,18 @@ export default class Setup extends Component {
   }
   async loadFonts() {
     await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-      Entypo: require("native-base/Fonts/Entypo.ttf"),
-      Feather: require("native-base/Fonts/Feather.ttf"),
-      FontAwesome: require("native-base/Fonts/FontAwesome.ttf"),
-      // MaterialIcons: require("native-base/Fonts/MaterialIcons.ttf"),
-      // MaterialCommunityIcons: require("native-base/Fonts/MaterialCommunityIcons.ttf"),
-      Octicons: require("native-base/Fonts/Octicons.ttf"),
-      // Zocial: require("@expo/vector-icons/fonts/Zocial.šttf"),
-      // SimpleLineIcons: require("native-base/Fonts/SimpleLineIcons.ttf"),
-      // EvilIcons: require("native-base/Fonts/EvilIcons.ttf"),
+      Roboto: require("../../node_modules/native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("../../node_modules/native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("../../node_modules/@expo/vector-icons/fonts/Ionicons.ttf"),
+      Entypo: require("../../node_modules/native-base/Fonts/Entypo.ttf"),
+      Feather: require("../../node_modules/native-base/Fonts/Feather.ttf"),
+      FontAwesome: require("../../node_modules/native-base/Fonts/FontAwesome.ttf"),
+      // MaterialIcons: require("../../node_modules/native-base/Fonts/MaterialIcons.ttf"),
+      // MaterialCommunityIcons: require("../../node_modules/native-base/Fonts/MaterialCommunityIcons.ttf"),
+      Octicons: require("../../node_modules/native-base/Fonts/Octicons.ttf"),
+      // Zocial: require("../../node_modules/@expo/vector-icons/fonts/Zocial.šttf"),
+      // SimpleLineIcons: require("../../node_modules/native-base/Fonts/SimpleLineIcons.ttf"),
+      // EvilIcons: require("../../node_modules/native-base/Fonts/EvilIcons.ttf"),
       // ...Ionicons.font,
     });
     this.setState({ isReady: true });
